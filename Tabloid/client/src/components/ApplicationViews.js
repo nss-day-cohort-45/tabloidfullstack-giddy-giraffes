c36c02d1cc751df680c3e915fcb76828f15c0911
Pazia Ramirez <pazia008@gmail.com>
2021-04-16 15:05:19 -0500

    moved category files in one folder

--- Tabloid/client/src/components/ApplicationViews.js
+++ Tabloid/client/src/components/ApplicationViews.js
@@ -5,8 +5,8 @@ import Login from "./Login";
 import Register from "./Register";
 import Hello from "./Hello";
 import UserList from "./UserProfileList";
-import CategoryList from "./CategoryList";
-import CategoryForm from "./CategoryForm";
+import CategoryList from "./Category/CategoryList";
+import CategoryForm from "./Category/CategoryForm";
 import TagList from "./TagList";
 import TagForm from "./TagForm";
 
