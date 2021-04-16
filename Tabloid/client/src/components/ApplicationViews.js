85670fc25e39fb3e1875fa4f25db549645ff3cdf
Pazia Ramirez <pazia008@gmail.com>
2021-04-16 10:22:13 -0500

    can view all categories

--- Tabloid/client/src/components/ApplicationViews.js
+++ Tabloid/client/src/components/ApplicationViews.js
@@ -24,20 +24,21 @@ export default function ApplicationViews() {
 
         <Route path="/register">
           <Register />
-
-          <Route path="/category">
-            {isLoggedIn ? <CategoryList /> : <Redirect to="/login" />}
-          </Route>
-
-
-          <Route path="/tag" exact>
-            {isLoggedIn ? <TagList /> : <Redirect to="/login" />}
-          </Route>
-
-          <Route path="/tag/add" exact>
-            {isLoggedIn ? <TagForm /> : <Redirect to="/login" />}
-          </Route>
         </Route>
+
+        <Route path="/category" exact>
+          {isLoggedIn ? <CategoryList /> : <Redirect to="/login" />}
+        </Route>
+
+
+        <Route path="/tag" exact>
+          {isLoggedIn ? <TagList /> : <Redirect to="/login" />}
+        </Route>
+
+        <Route path="/tag/add" exact>
+          {isLoggedIn ? <TagForm /> : <Redirect to="/login" />}
+        </Route>
+
       </Switch>
     </main>
   );
